83e07185b0c4ea58ae7d641ea6c16e6f2186852d
Amin <aminehidri44@gmail.com>
2023-09-21 16:53:04 +0100

    Updated paddings, replaced margin with gap.

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -3,18 +3,17 @@ import React from "react"
 
 function BlogPost({ blogPosts }) {
   return (
-    <div className="flex flex-col flex-wrap items-center">
+    <div className="flex flex-col gap-8 flex-wrap items-center">
       {blogPosts.map((post) => (
         <div
           key={post.id}
-          className="w-1/3 border border-black m-5 flex items-center flex-col"
+          className="w-1/3 border p-3 border-black  flex items-center flex-col"
         >
-          {" "}
           <Link to={post.to}>
             <img
               src={post.image}
               alt="Image"
-              className="object-cover h-48 w-96 p-3"
+              className="object-cover h-48 w-96"
             />
             <h2 className="text-black text-2xl">{post.title}</h2>
             <p className="text-black p-2">{post.content}</p>
