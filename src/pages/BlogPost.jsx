152e46660391add0b88a5c35e3679c2127f8b9a4
Amin <aminehidri44@gmail.com>
2023-09-28 22:41:38 +0100

    Fixed md and lg responsiveness on BlogPost

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -7,19 +7,17 @@ function BlogPost({ blogPosts }) {
       {blogPosts.map((post) => (
         <div
           key={post.id}
-          className="w-80 md:w-1/3 border p-3 border-black hover:shadow-black hover:shadow-sm hover:duration-150 duration-150 hover:transition-shadow flex items-center flex-col"
+          className="w-80 lg:w-96 border p-3 border-black hover:shadow-black hover:shadow-sm hover:duration-150 duration-150 hover:transition-shadow flex items-center flex-col"
         >
           <Link to={post.to} class="flex flex-col items-center">
             <img
               src={post.image}
               alt="Image"
-              className="object-cover h-24  md:h-48 w-72 md:w-96"
+              className="object-cover h-24  md:h-48 w-72 md:w-80 lg:h-48 lg:w-96"
             />
             <h2 className="text-black text-2xl pt-2 pb-1">{post.title}</h2>
             <p className="text-black w-72 md:w-100 p-2">{post.content}</p>
-            <div className="md:w-96 text-gray-400 md:pb-2 md:pl-1 md:text-left">
-              {post.date}
-            </div>{" "}
+            <div className=" text-gray-400">{post.date}</div>{" "}
           </Link>
         </div>
       ))}
