c366c0b748d71f146555f7eb1b3b1c6e7c14bb9e
Amin <aminehidri44@gmail.com>
2023-09-28 19:08:14 +0100

    Worked on BlogPost.jsx responsiveness

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -7,17 +7,17 @@ function BlogPost({ blogPosts }) {
       {blogPosts.map((post) => (
         <div
           key={post.id}
-          className="w-1/3 border p-3 border-black hover:shadow-black hover:shadow-sm hover:duration-150 duration-150 hover:transition-shadow flex items-center flex-col"
+          className="w-80 md:w-1/3 border p-3 border-black hover:shadow-black hover:shadow-sm hover:duration-150 duration-150 hover:transition-shadow flex items-center flex-col"
         >
-          <Link to={post.to}>
+          <Link to={post.to} class="flex flex-col items-center">
             <img
               src={post.image}
               alt="Image"
-              className="object-cover h-48 w-96"
+              className="object-cover h-24  md:h-48 w-72 md:w-96"
             />
             <h2 className="text-black text-2xl pt-2 pb-1">{post.title}</h2>
-            <p className="text-black p-2">{post.content}</p>
-            <div className="w-96 text-gray-400 pb-2 pl-1 text-left">
+            <p className="text-black w-72 md:w-100 p-2">{post.content}</p>
+            <div className="md:w-96 text-gray-400 md:pb-2 md:pl-1 md:text-left">
               {post.date}
             </div>{" "}
           </Link>
