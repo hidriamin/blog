fd7cac75b8adcc28124766fa1def3f67e6bc1240
Amin <aminehidri44@gmail.com>
2023-09-28 23:10:53 +0100

    Updated Header.jsx reponsiveness

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,15 +5,13 @@ function Header() {
   return (
     <div className="border-b pb-3">
       <h1 className="text-black text-3xl p-3 pb-10">Welcome to my blog</h1>
-      <div className="flex flex-row justify-center ">
+      <div className="flex flex-row justify-center">
         {" "}
-        <div className="text-center items-center  gap-3 md:gap-0 w-40 md:w-100 md:text-start flex flex-col md:flex-row">
-          <div className="flex flex-col ">
-            <img className="object-cover h-48 w-48 pr-3" src={profilePhoto} />{" "}
-          </div>
-          <div className="flex flex-col items-center">
+        <div className="text-center items-center gap-3 md:gap-0 w-40 md:w-80 md:text-start md:items-start flex flex-col md:flex-row">
+          <img className="object-cover h-48 w-48 pr-3" src={profilePhoto} />{" "}
+          <div className="flex flex-col items-center md:items-start">
             <h2 className="text-black text-2xl">Who am i?</h2>
-            <p className="text-black  w-80 text-center md:w-96 pr-0 md:pr-3">
+            <p className="text-black  w-80 text-center md:text-start md:w-72 pr-0">
               I'm a Marketing graduate who discovered he's into programming.
               Basically, i do web development. This website is part of my work.
               You can find my main website{" "}
@@ -26,18 +24,19 @@ function Header() {
               </a>{" "}
               .
             </p>{" "}
-          </div>{" "}
-          <div className="flex flex-col items-center">
-            <div className="text-black text-start justify-start ">
-              <h2 className="text-black text-2xl text-center md:text-start">
-                About this website:
-              </h2>
-              <p className="text-black  w-80 text-center md:text-start md:w-96 ">
-                This is a minimalist Blog website that uses no third party blog
-                software.Instead, it was built with ReactJS and tailwindcss.
-              </p>
+            <div className="flex flex-col items-center">
+              <div className="text-black text-start justify-start ">
+                <h2 className="text-black text-2xl text-center md:text-start">
+                  About this website:
+                </h2>
+                <p className="text-black  w-80 md:w-72 text-center md:text-start ">
+                  This is a minimalist Blog website that uses no third party
+                  blog software.Instead, it was built with ReactJS and
+                  tailwindcss.
+                </p>
+              </div>
             </div>
-          </div>
+          </div>{" "}
         </div>
       </div>
     </div>
