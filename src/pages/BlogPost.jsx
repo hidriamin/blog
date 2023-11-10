6dbb05b79d9bbd464b65d478526406ab74ed5f81
Amin <aminehidri44@gmail.com>
2023-11-10 11:26:31 +0100

    Improved responsiveness on md screen size

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -7,16 +7,16 @@ function BlogPost({ blogPosts }) {
       {blogPosts.map((post) => (
         <div
           key={post.id}
-          className="w-72 xsm:w-48 lg:w-96  border p-3 border-black hover:shadow-black hover:shadow-sm hover:duration-150 duration-150 hover:transition-shadow"
+          className="w-72 xsm:w-48 lg:w-96 md:w-2/3 border p-3 border-black hover:shadow-black hover:shadow-sm hover:duration-150 duration-150 hover:transition-shadow"
         >
           <Link to={post.to} class="flex flex-col items-center">
             <img
               src={post.image}
               alt="Image"
-              className="object-cover h-24  md:h-48 w-72 md:w-80 lg:h-48 lg:w-96"
+              className="object-cover h-24  md:h-48 w-72 md:w-96 lg:h-48 lg:w-96"
             />
             <h2 className="text-black text-2xl pt-2 pb-1">{post.title}</h2>
-            <p className="text-black w-72 md:w-100 xsm:w-48 p-2">
+            <p className="text-black w-72 md:w-96 xsm:w-48 p-2">
               {post.content}
             </p>
             <div className=" text-gray-400">{post.date}</div>{" "}
